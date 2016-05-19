9d00739c80253a826f79c50cbefa1aed62ca9fb5
Your Name <you@example.com>
2016-05-19 23:25:06 +0300

    initial commit with working map and working population

---
+++ js/main.js
@@ -0,0 +1,322 @@
+var cities = [
+	{
+		"city_name": "Driftveil City",
+		"code": "driftveil_city",
+		"population": "70",
+		"important_loc": [
+			{
+				"location_name": "Driftveil City Gym",
+				"gym_type": "Ground",
+				"gym_leader": "Clay"
+			},
+			{
+				"location_name": "move tutor",
+				"required": "red shards",
+				"moves": [
+					"Covet",
+					"Bug Bite",
+					"Drill Run",
+					"Bounce",
+					"Signal Beam",
+					"Iron Head",
+					"Super Fang",
+					"Uproad",
+					"Seed Bomb",
+					"Dual Chop",
+					"Low Kick",
+					"Gunk Shot",
+					"Fire Punch",
+					"Thunder Punch",
+					"Ice Punch"
+				]
+			},
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "driftveil market"
+			}
+		]
+	},
+	{
+		"city_name": "Mistralton City",
+		"code": "mistralton_city",
+		"population": "31",
+		"important_loc": [
+			{
+				"location_name": "Mistralton City Gym",
+				"gym_type": "Flying",
+				"gym_leader": "Skyla"
+			},
+			{
+				"location_name": "cargo service"
+			},
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "TM collector"
+			}
+		]
+	},
+	{
+		"city_name": "Icirrus City",
+		"code": "icirrus_city",
+		"population": "26",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "pokemon fan club"
+			}
+		]
+	},
+	{
+		"city_name": "Opelucid City",
+		"code": "opelucid_city",
+		"population": "52",
+		"important_loc": [
+			{
+				"location_name": "Opelucid City Gym",
+				"gym_type": "Dragon",
+				"gym_leader": "Drayden"
+			},
+			{
+				"location_name": "pokemon center"
+			}
+		]
+	},
+	{
+		"city_name": "Lacunosa Town",
+		"code": "lacunosa_town",
+		"population": "16",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			}
+		]
+	},
+	{
+		"city_name": "Humilau City",
+		"code": "humilau_city",
+		"population": "32",
+		"important_loc": [
+			{
+				"location_name": "Humilau City Gym",
+				"gym_type": "Water",
+				"gym_leader": "Marlon"
+			},
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "move tutor",
+				"required": "yellow shards",
+				"moves": [
+					"Bind",
+					"Snore",
+					"Heal Bell",
+					"Knock Off",
+					"Synthesis",
+					"Roost",
+					"Sky Attack",
+					"Role Play",
+					"Heat Wave",
+					"Giga Drain",
+					"Pain Split",
+					"Tailwind"
+				]
+			}
+		]
+	},
+	{
+		"city_name": "Undella Town",
+		"code": "undella_town",
+		"population": "15",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "caitlin's villa"
+			}
+		]
+	},
+	{
+		"city_name": "Lentimas Town",
+		"code": "lentimas_town",
+		"population": "12",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "move tutor",
+				"required": "yellow shards",
+				"moves": [
+					"Last Resort",
+					"Iron Defense",
+					"Magnet Rise",
+					"Magic Coat",
+					"Block",
+					"Hyper Voice",
+					"Electro Web",
+					"Icy Wind",
+					"Iron Tail",
+					"Aqua Tail",
+					"Earth Power",
+					"Zen Headbutt",
+					"Foul Play",
+					"Superpower",
+					"Gravity",
+					"Dragon Pulse",
+					"Dark Pulse"
+				]
+			}
+		]
+	},
+	{
+		"city_name": "Nimbasa City",
+		"code": "nimbasa_city",
+		"population": "239",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "Nimbasa City Gym",
+				"gym_type": "Electric",
+				"gym_leader": "Elesa"
+			},
+			{
+				"location_name": "musical theater"
+			},
+			{
+				"location_name": "battle subway"
+			},
+			{
+				"location_name": "big stadium"
+			},
+			{
+				"location_name": "small court"
+			},
+			{
+				"location_name": "big stadium"
+			}
+		]
+	},
+	{
+		"city_name": "Castelia City",
+		"code": "castelia_city",
+		"population": "127",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "Castelia City Gym",
+				"gym_type": "Bug",
+				"gym_leader": "Burgh"
+			},
+			{
+				"location_name": "royal unova"
+			},
+			{
+				"location_name": "passerby analytics"
+			},
+			{
+				"location_name": "medal office"
+			},
+			{
+				"location_name": "name-rating service"
+			}
+		]
+	},
+	{
+		"city_name": "Aspertia City",
+		"code": "aspertia_city",
+		"population": "32",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "Aspertia City Gym",
+				"gym_type": "Normal",
+				"gym_leader": "Cheren"
+			},
+			{
+				"location_name": "trainer's school"
+			}
+		]
+	},
+	{
+		"city_name": "Virbank City",
+		"code": "virbank_city",
+		"population": "40",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "Virbank City Gym",
+				"gym_type": "Poison",
+				"gym_leader": "Roxie"
+			}
+		]
+	},
+	{
+		"city_name": "Floccessy Town",
+		"code": "floccessy_town",
+		"population": "17",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			}
+		]
+	},
+	{
+		"city_name": "Victory Road",
+		"code": "victory_road",
+		"population": "?",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			}
+		]
+	},
+	{
+		"city_name": "Pokemon League",
+		"code": "pokemon_league",
+		"population": "17",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			}
+		]
+	}
+];
+
+$(document).ready(function () {
+	var map = $('div.map');
+	$('#cities circle').click(function (e) {
+		$('.info-panel').remove();
+		var region_code = $(this).attr('id');
+		$.each(cities, function (index, value) {
+			if (value["code"] === region_code) {
+				console.log(region_code);
+				$('<div class="info-panel">' + value["city_name"] + '<br/>' + 'Population: ' + value["population"] + '</div>').appendTo(map);
+			}
+		});
+		var mouseX = e.pageX;
+		var mouseY = e.pageY;
+		$('.info-panel').css({
+			top: mouseY - 100,
+			left: mouseX - ($(this).width() / 2)
+		})
+	})
+	.dblclick(function () {
+		$('.info-panel').remove();
+	}); 
+});
