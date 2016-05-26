9f53075cd7aaefc2e9460832677b8ca3a8e0074a
Your Name <you@example.com>
2016-05-26 18:10:08 +0300

    added bootstrap and added mini cities

--- js/main.js
+++ js/main.js
@@ -153,7 +153,7 @@ var cities = [
 			},
 			{
 				"location_name": "move tutor",
-				"required": "yellow shards",
+				"required": "blue shards",
 				"moves": [
 					"Last Resort",
 					"Iron Defense",
@@ -289,40 +289,375 @@ var cities = [
 	{
 		"city_name": "Pokemon League",
 		"code": "pokemon_league",
-		"population": "17",
+		"population": "?",
 		"important_loc": [
 			{
 				"location_name": "pokemon center"
 			}
 		]
+	},
+	{
+		"city_name": "Nacrene City",
+		"code": "nacrene_city",
+		"population": "54",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "nacrene museum"
+			},
+			{
+				"location_name": "move tutor",
+				"required": "green shards",
+				"moves": [
+					"Worry Seed",
+					"Gastro Acid",
+					"Helping Hand",
+					"After You",
+					"Magic Room",
+					"Wonder Room",
+					"Spite",
+					"Recycle",
+					"Trick",
+					"Stealth Rock",
+					"Outrage",
+					"Endeavor",
+					"Sleep Talk",
+					"Skill Swap",
+					"Snatch"
+				]
+			},
+			{
+				"location_name": "café warehouse"
+			}
+		]
+	},
+	{
+		"city_name": "Striaton City",
+		"code": "striaton_city",
+		"population": "46",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			},
+			{
+				"location_name": "striaton restaurant"
+			},
+			{
+				"location_name": "trainers' school"
+			}
+		]
+	},
+	{
+		"city_name": "Accumula Town",
+		"code": "accumula_town",
+		"population": "36",
+		"important_loc": [
+			{
+				"location_name": "pokemon center"
+			}
+		]
+	},
+	{
+		"city_name": "Nuvema Town",
+		"code": "nuvema_town",
+		"population": "10",
+		"important_loc": [
+			{
+				"location_name": "juniper pokemon lab"
+			}
+		]
+	},
+	{
+		"city_name": "PWT",
+		"code": "pwt",
+		"population": "?",
+		"important_loc": [
+			{
+				"location_name": "juniper pokemon lab"
+			}
+		]
+	},
+	{
+		"city_name": "Pokestar Studios",
+		"code": "pokestar_studio",
+		"population": "?",
+		"important_loc": [
+			{
+				"location_name": "pokestar studio"
+			}
+		]
+	},
+	{
+		"city_name": "Join Avenue",
+		"code": "join_avenue",
+		"population": "?",
+		"important_loc": [
+			{
+				"location_name": "join avenue"
+			}
+		]
+	}
+];
+
+var places = [
+	{
+		"place_name": "Cave of Being",
+		"code": "cave_of_being",
+		"description": "They say the deepest part of the cave leads to the Sinnoh region.",
+		"wild_pokemons": [
+			
+		]
+	},
+	{
+		"place_name": "Virbank Complex",
+		"code": "virbank_complex",
+		"description": "A complex that is designed so Pokemon can work there easily.",
+		"wild_pokemons": [
+			
+		]
+	},
+	{
+		"place_name": "Floccesy Ranch",
+		"code": "flocessy_ranch",
+		"description": "Before long, the area became a ranch as Pokemon and poeple gathered.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Chargestone Cave",
+		"code": "chargestone_cave",
+		"description": "A cave where electrically charged stones float.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Anville Town",
+		"code": "anville_town",
+		"description": "A city with a rail yard where trains park for an overhaul.",
+		"wild_pokemons": [
+			
+		]
+	},
+	{
+		"place_name": "Chargestone Cave",
+		"code": "chargestone_cave",
+		"description": "A cave where electrically charged stones float.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Celestial Tower",
+		"code": "celestial_twr",
+		"description": "A tall tower with a large bell whose tones are said to purify the spirit.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Mistralton Cave",
+		"code": "mistralton_cave",
+		"description": "A forgotten cave sealed with hints of a legend's presence.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Clay Tunnel",
+		"code": "clay_tunnel",
+		"description": "A tunnel created by Clay's constant mining. It leads to Twist Mountain.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Twist Mountain",
+		"code": "twist_mountain",
+		"description": "It has a reputation as a mine littered with valuable ore.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Dragonspiral Tower",
+		"code": "dragonspiral_twr",
+		"description": "The oldest tower in the Unova region. No one knows its history.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Moor of Icirrus",
+		"code": "moor_of_icirrus",
+		"description": "They say water collecting in hollows on the flat land created the moor.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Chargestone Cave",
+		"code": "chargestone_cave",
+		"description": "A cave where electrically charged stones float.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Liberty Garden",
+		"code": "liberty_garden",
+		"description": "An island symbol of hope for a world where humans and Pokemon live free.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Relic Passage",
+		"code": "relic_passage",
+		"description": "A recently discovered passage that leads to the Relic Castle.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Castelia Sewers",
+		"code": "castelia_sewers",
+		"description": "Iron bars around the exits were dismantled, drawing curious Trainers.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Entralink",
+		"code": "entralink",
+		"description": "In the middle of the Unova region, it is a place of mysterious power.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Lostlorn Forst",
+		"code": "lostlorn_forest",
+		"description": "It was once known as a place where people got lost for no reason.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Relic Castle",
+		"code": "relic_castle",
+		"description": "Ancient ruins with a glorious history, buried in the sand as time went by.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Poke Transfer Lab",
+		"code": "poketransfer_lab",
+		"description": "A facility to study Pokemon from faraway regions.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Abundant Shrine",
+		"code": "abundant_shrine",
+		"description": "People once lived in this village, and it still feels the same as it did then.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Reversal Mountain",
+		"code": "reversal_mountain",
+		"description": "A volvano with a record of a huge eruption long ago.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Strange House",
+		"code": "strange_house",
+		"description": "A house known for a sad incident that is said to keep people away.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Pinwheel Forest",
+		"code": "pinwheel_forest",
+		"description": "You can go through hollows of fallen trees, too.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Wellspring Cave",
+		"code": "wellspring_cave",
+		"description": "This underwater pool grew from rainwater soaking into the ground.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Giant Chasm",
+		"code": "giant_chasm",
+		"description": "Legend says if you approach this big chasm, disasters surely follow.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Seaside Cave",
+		"code": "seaside_cave",
+		"description": "A rock that became passable due to wind, waves, and Pokemon.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "Dreamyard",
+		"code": "dreamyard",
+		"description": "A plant site used as a playground for children and Pokemon.",
+		"wild_pokemons": [
+
+		]
+	},
+	{
+		"place_name": "P2 Laboratory",
+		"code": "p2_laboratory",
+		"description": "A mysterious black sailing vessel seen in various locations in Unova.",
+		"wild_pokemons": [
+
+		]
 	}
 ];
 
 $(document).ready(function () {
-	var map = $('div.map');
-	$('#cities circle').click(function (e) {
+	
+	$('.map circle:not(.flyport)').click(function (e) {
 		$('.info-panel').remove();
-		var region_code = $(this).attr('id');
+		var region_code = $(this).attr('id').toLowerCase();
 		var locations = "";
 		$.each(cities, function (index, value) {
 			if (value["code"] === region_code) {
+				$('.locName').text(value["city_name"]);				
+				$('.population').text(value["population"]);
+				
+				$('.important-locations').empty();
 				$.each(value["important_loc"], function (index, value) {
-					locations += value["location_name"] + ", ";
+					
+					$('.important-locations').append("<div><p>" + value["location_name"] + "</p></div>");
 				});
-				locations = locations.substring(0, locations.length - 2);
-				$('<div class="info-panel">' + value["city_name"] + '<br/>' + 'Population: ' + value["population"] + '<br/>' + '<h3>Important Locations: ' + locations + '</h3></div>').appendTo(map);
-				console.log(region_code);
-				console.log(locations);
 			}
 		});
-		var mouseX = e.pageX;
-		var mouseY = e.pageY;
-		$('.info-panel').css({
-			top: mouseY - 100,
-			left: mouseX - ($(this).width() / 2)
-		})
-	})
-	.dblclick(function () {
-		$('.info-panel').remove();
-	}); 
+	});
+	
+	$('.map circle.flyport').click(function (e) {
+		console.log("what is this place");
+	});
+	 
 });
